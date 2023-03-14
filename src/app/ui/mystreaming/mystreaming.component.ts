d38b71d079c1f63de939b686074208a1e5c29929
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-03-14 15:05:20 -0600

    User Module Complete

--- src/app/ui/mystreaming/mystreaming.component.ts
+++ src/app/ui/mystreaming/mystreaming.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ApiStreamingService } from '../../service/apistreaming.service';
+import { ApiauthService } from 'src/app/service/apiauth.service';
 
 @Component({
   selector: 'app-mystreaming',
@@ -9,14 +10,17 @@ import { ApiStreamingService } from '../../service/apistreaming.service';
 })
 export class MystreamingComponent implements OnInit {
 
+  public usuario = this._apiauth.usuarioData;
+
   public lstStreaming: any[] = [];
 
   constructor(
-    private _apistreaming: ApiStreamingService
+    private _apistreaming: ApiStreamingService,
+    private _apiauth: ApiauthService
   ) { }
 
   ngOnInit(): void {
-    this.getStreamings(1);
+    this.getStreamings(this.usuario.idUsuario);
   }
 
   getStreamings(user: number) {
